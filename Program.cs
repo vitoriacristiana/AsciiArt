4676d921fe072b8f5b760d59f8e7674a27a7d748
Vitória Cristiana <vitoriacristiana290@gmail.com>
2021-08-24 17:17:36 -0300

    Final

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AsciiArt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ||  || ");
+            Console.WriteLine(" \\\\()// ");
+            Console.WriteLine("//(__)\\\\");
+            Console.WriteLine("||    ||");
+        }
+    }
+}
